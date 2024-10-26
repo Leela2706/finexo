19983d05149fceda3139b869767c6ea1d568337c
leela2706 <saileelavardhan12@gmail.com>
2024-10-26 17:46:12 +0530

    Finexo project

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+  render() {
+    return <div>
+  
+  {/* footer section */}
+  <section className="footer_section">
+    <div className="container">
+      <p>
+        © <span id="displayYear" /> All Rights Reserved By
+        <a href="https://html.design/">Free Html Templates</a>
+      </p>
+    </div>
+  </section>
+  {/* footer section */}
+  
+</div>
+
+  }
+}
